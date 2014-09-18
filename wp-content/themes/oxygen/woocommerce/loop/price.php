aaabc3d88346754ed14476680a21f9ef4f49e184
juan2ramos <juan2ramos@gmail.com>
2014-09-18 17:18:00 -0500

    loop

--- wp-content/themes/oxygen/woocommerce/loop/price.php
+++ wp-content/themes/oxygen/woocommerce/loop/price.php
@@ -13,29 +13,29 @@ global $product;
 ?>
 
 <?php if ( $price_html = $product->get_price_html() ) : ?>
-	<span class="price"><?php echo $price_html; ?></span>
+    <span class="price"><?php echo $price_html; ?></span>
 <?php endif; ?>
 
 <?php
 # start: modified by Arlind Nushi
 if( ! get_data('shop_add_to_cart_listing'))
-	return;
+    return;
 # end: modified by Arlind Nushi
 ?>
 
 <?php if(is_catalog_mode()) return; ?>
 
 <?php if($product->is_type('variable')): ?>
-<a class="add-to-cart-btn entypo-list-add" data-toggle="tooltip" data-placement="bottom" title="<?php _e('Select Options', TD); ?>" href="<?php echo $product->get_permalink(); ?>"></a>
+    <a class="add-to-cart-btn entypo-list-add" data-toggle="tooltip" data-placement="bottom" title="<?php _e('Select Options', TD); ?>" href="<?php echo $product->get_permalink(); ?>"></a>
 
 <?php elseif($product->is_type('grouped')): ?>
-<a class="add-to-cart-btn entypo-list-add" data-toggle="tooltip" data-placement="bottom" title="<?php _e('Select Products', TD); ?>" href="<?php echo $product->get_permalink(); ?>"></a>
+    <a class="add-to-cart-btn entypo-list-add" data-toggle="tooltip" data-placement="bottom" title="<?php _e('Select Products', TD); ?>" href="<?php echo $product->get_permalink(); ?>"></a>
 
 <?php elseif($product->is_type('external')): ?>
-<a class="add-to-cart-btn entypo-export" data-toggle="tooltip" data-placement="bottom" title="<?php echo $product->single_add_to_cart_text(); ?>" href="<?php echo $product->get_product_url(); ?>" target="_blank"></a>
+    <a class="add-to-cart-btn entypo-export" data-toggle="tooltip" data-placement="bottom" title="<?php echo $product->single_add_to_cart_text(); ?>" href="<?php echo $product->get_product_url(); ?>" target="_blank"></a>
 
-<?php else: wc_get_template();?>
-<a class="add-to-cart-btn add-to-cart iconmm-cart" data-id="<?php echo $product->post->ID; ?>" data-toggle="tooltip" data-placement="bottom" title="<?php _e('Agregar al Carrito', TD); ?>" href="#">
-	<span class=" "></span>
-</a>
+<?php else: ?>
+    <a class="add-to-cart-btn add-to-cart iconmm-basket-5" data-id="<?php echo $product->post->ID; ?>" data-toggle="tooltip" data-placement="bottom" title="<?php _e('Agregar al Carrito', TD); ?>" href="#">
+        <span class=" "></span>
+    </a>
 <?php endif; ?>
