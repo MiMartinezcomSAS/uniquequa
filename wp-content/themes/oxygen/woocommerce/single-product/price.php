49f525fdc42b304fbead74a8849c913bd0673274
Juan <juan2ramos@gmail.com>
2014-11-14 15:48:31 -0500

    Update price.php

--- wp-content/themes/oxygen/woocommerce/single-product/price.php
+++ wp-content/themes/oxygen/woocommerce/single-product/price.php
@@ -31,7 +31,9 @@ if($product->is_type('variable'))
 <div class="<?php echo $hide_main_offer ? 'price-hidden' : ''; ?>" itemprop="offers" itemscope itemtype="http://schema.org/Offer">
 
 	<p class="price"><?php echo $product->get_price_html(); ?></p>
-
+	<div class="impuestos">
+	        <?php echo get_post_meta($post->ID, 'impuestos', true); ?>
+	    </div>
 	<meta itemprop="price" content="<?php echo $product->get_price(); ?>" />
 	<meta itemprop="priceCurrency" content="<?php echo get_woocommerce_currency(); ?>" />
 	<link itemprop="availability" href="http://schema.org/<?php echo $product->is_in_stock() ? 'InStock' : 'OutOfStock'; ?>" />
